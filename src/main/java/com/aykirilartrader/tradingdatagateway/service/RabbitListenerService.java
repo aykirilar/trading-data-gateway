9254679e6332d6e02551855c61197ea5cbef2d69
MehmetHikmetDemirci <mehmethikmetdemirci1@gmail.com>
2024-02-21 20:43:28 +0300

    class renamed

--- src/main/java/com/aykirilartrader/tradingdatagateway/service/RabbitListenerService.java
+++ src/main/java/com/aykirilartrader/tradingdatagateway/service/RabbitListenerService.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RabbitListernerService {
+public class RabbitListenerService {
 
     @RabbitListener(queues = "trademessages")
     public void receiveMessage(String message) {
