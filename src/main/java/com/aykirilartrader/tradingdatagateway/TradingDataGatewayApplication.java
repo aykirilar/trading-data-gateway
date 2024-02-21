4511d810ef00f3a9218fc4fcacb81c701580f98b
MehmetHikmetDemirci <mehmethikmetdemirci1@gmail.com>
2024-02-21 18:50:05 +0300

    initial commit

---
+++ src/main/java/com/aykirilartrader/tradingdatagateway/TradingDataGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aykirilartrader.tradingdatagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDataGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingDataGatewayApplication.class, args);
+	}
+
+}
